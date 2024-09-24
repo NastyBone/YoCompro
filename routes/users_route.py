43fd47ad2d7473474a50ed077d508d7e40545074
José <jose.c.prieto.c2015@gmail.com>
2024-09-24 11:09:08 -0400

    minimal test on user endpoints

--- routes/users_route.py
+++ routes/users_route.py
@@ -1,10 +1,12 @@
 from helpers import roles_list
 from guard import role_required
 from flask import Blueprint, request, jsonify, render_template
-from flask_login import current_user, login_required
+from flask_login import current_user
 from services.lists_services import get_by_user as list_user
 from services.ratings_service import get_by_user as ratings_user
 from services.users_service import *
+from guard import login_required, secure_access
+
 users_bp = Blueprint("users", __name__)
 
 
@@ -55,19 +57,24 @@ def remove():
 # CLIENT
 
 @users_bp.route('/become-owner', methods=['GET'])
+@login_required()
 def become_owner():
+    # TODO: check if user is already an owner
+    # TODO: logic for create bussiness
     return render_template('create_owner')
 
 
-@users_bp.route('/profile-', methods=['GET'])
+@users_bp.route('/profile-edit', methods=['GET'])
+@login_required()
 def edit_profile():
     return render_template('edit_profile')
 
 
 @users_bp.route('/my-list', methods=['GET'])
+@secure_access()
 def get_list():
-    response = list_user(current_user.get_id())
-    return render_template('', list=response)
+    response = list_user(request.args.get('user_id'))
+    return jsonify(response)
 
 
 @users_bp.route('/my-ratings', methods=['GET'])
