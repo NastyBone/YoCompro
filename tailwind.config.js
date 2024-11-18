dd209d6665dd6cf41d6027c176d8d0d66702f97d
Jose Prieto <jose.c.prieto.c2015@gmail.com>
2024-11-18 00:19:41 -0400

    client_dashboard done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors")
 module.exports = {
   content: ["./templates/**/*.{html,js}", "./templates/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      ...colors,
+      baseblue: "#0d6efd",
+    },
   },
   plugins: [],
+  prefix: "tw-",
 }
