8560bfdadf708a84a0fffd1fe6cc413e2064d6d3
José Prieto <42517974+NastyBone@users.noreply.github.com>
2025-02-21 14:50:14 -0400

    feat: enhance admin search functionality, add filter options, and improve CSS styles

--- routes/admin_route.py
+++ routes/admin_route.py
@@ -7,6 +7,8 @@ from services.stats_service import (
 )
 from helpers import set_pagination, status_list, type_list
 from services.products_service import get_by_status as products_by_status
+from services.bussiness_service import get_by_status as bussinnes_by_status
+from services.tags_service import get_by_status as tags_by_status
 
 # TODO: Protect
 admin_bp = Blueprint("admin", __name__)
@@ -18,23 +20,36 @@ def stats():
     users_stats = users()
     products_stats = products()
     tags_stats = tags()
-    return jsonify(
-        {
-            "bussiness": bussiness_stats,
-            "users": users_stats,
-            "products": products_stats,
-            "tags": tags_stats,
-        }
+    return render_template(
+        "dashboards/admin_dashboard.html",
+        bussiness=bussiness_stats,
+        users=users_stats,
+        products=products_stats,
+        tags=tags_stats,
     )
 
 
 @admin_bp.route("/search", methods=["GET"])
 def general_search():
 
+    type = request.args.get("type", "products")
     [start_pagination, end_pagination] = set_pagination(None)
-    [response, count] = products_by_status(
-        "approved", start_pagination, end_pagination, ""
-    )
+    if type == "products":
+        [response, count] = products_by_status(
+            "approved", start_pagination, end_pagination, ""
+        )
+    elif type == "bussiness":
+        [response, count] = bussinnes_by_status(
+            "approved", start_pagination, end_pagination, ""
+        )
+    elif type == "tags":
+        [response, count] = tags_by_status(
+            "approved", start_pagination, end_pagination, ""
+        )
+    else:
+        [response, count] = products_by_status(  # users by status
+            "approved", start_pagination, end_pagination, ""
+        )
 
     return render_template(
         "search/admin_search.html",
